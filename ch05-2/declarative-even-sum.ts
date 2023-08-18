2e0539395067afc71108c0a5e6cd66d16927fded
suhyunking <kksshh09211@gmail.com>
2023-08-18 16:53:04 +0900

    짝수 합 구하기

---
+++ ch05-2/declarative-even-sum.ts
@@ -0,0 +1,10 @@
+import { range } from '../ch05-1/range'
+import { fold } from './fold'
+import { filter } from './filter'
+
+let numbers: number[] = range(1, 100 + 1)
+const isEven = (n: number): boolean => n % 2 == 0
+let result = fold(
+    filter(numbers, isEven),
+    (result, value) => result + value, 0)
+console.log(result) // 2550
---
+++ ch05-2/imperative-even-sum.ts
@@ -0,0 +1,5 @@
+//1에서 100까지 짝수의 합 구하기
+let evenSum = 0
+for(let val = 0; val <= 100; val += 2)
+    evenSum += val
+console.log(evenSum)//2550
